81f04f43a4579b797987783d64ea8702176decc5
Rio <9rio10089@gmail.com>
2026-02-07 23:42:03 +0900

    242 Valid Anagram

--- Roadmap/01-Arrays-Hashing/242-Valid-Anagram.py
+++ Roadmap/01-Arrays-Hashing/242-Valid-Anagram.py
@@ -1,5 +1,27 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        # create a frequency hash map for both s and t
+        # count the occurrences of each character and compare the two maps
+        # if the maps are identical, return true; otherwise, return false 
+        map_s, map_t = {}, {}  
+        for i in range(len(s)):
+            if s[i] not in map_s:
+                map_s[s[i]] = 0
+            if t[i] not in map_t:
+                map_t[t[i]] = 0
+                
+            map_s[s[i]] += 1
+            map_t[t[i]] += 1
+
+        return map_s == map_t
+    
+    # time complexity: O(n)
+    # space complexity: O(n)
+    
+    def isAnagram2(self, s: str, t: str) -> bool:
         # first of all inputs are rejected if lengths don't match
         if len(s) != len(t):
             return False
