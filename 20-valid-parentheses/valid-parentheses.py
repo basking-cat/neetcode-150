c8c74b47b33b31df9b043d0011890c5419a290c7
Rio Hazama <prog.rio1101@gmail.com>
2026-03-22 14:16:22 +0900

    Time: 3 ms (34.44%) | Memory: 19.3 MB (84.84%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        # use stack and hashmap
+        # if the character we're currently looking at is a closing bracket, we look up the hashmap and compare it to stack.pop()
+        # if stack is not empty at the end or empty before reading all the characters, the input is not valid
+
+        stack = []
+        hashmap = {')': '(', '}': '{', ']': '['}
+
+        for c in s:
+            if c not in hashmap: # append open brackets
+                stack.append(c)
+            if c in list(hashmap.keys()):
+                if not stack or stack.pop() != hashmap[c]:
+                    return False
+
+        if not stack:
+            return True
+        else:
+            return False
+        
